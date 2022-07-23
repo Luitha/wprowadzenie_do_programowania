2024e5fcb98387ab89628022723e640037f7ffa9
Luitha <bardat.87@gmail.com>
2022-07-23 11:26:12 +0200

    dodałam zmienną o nazwie wiek

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String name = "Barbara";
+        int age = 35;
+        System.out.println("Hello, " + "my name is " +name +"!" +" I am " +age +" years old.");
+
     }
 }
