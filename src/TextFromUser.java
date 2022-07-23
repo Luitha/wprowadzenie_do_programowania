5c4c0691d555e6804e969a2cea20b93909aa90b4
Luitha <bardat.87@gmail.com>
2022-07-23 12:04:49 +0200

    dodałam zmienną o nazwie wiek

--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class TextFromUser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        System.out.println("witaj użytkowniku " +userInput);
+        System.out.println("Podaj imię ");
+        String imie = scanner.nextLine();
+        System.out.println("Podaj nazwisko ");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Podaj wiek ");
+        int age = scanner.nextInt();
+        System.out.println("Witaj, " +imie +" " +nazwisko +" " +age);
     }
 }
